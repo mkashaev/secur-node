7fd90ad4352add74ea462583e937746409071958
Кашаев Марат <kasaevmarat@MacBook-Pro-x5retailgrnv.local>
2021-02-01 01:58:56 +0300

    ADD comments for lab

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use("/utils", utilsRouter);
 
 module.exports = app;
 
-/*
+/* 4
 Guys, a found cheaper and better, good luck
 <form action="/checkout" method="post">
   <input hidden name="city" value="423" />
@@ -42,5 +42,35 @@ Guys, a found cheaper and better, good luck
   <input hidden name="building" value="1" />
   <input hidden name="appartment" value="1" />
   <button type="submit" class="btn btn-link">http://aliexpress.com/best/cheap/iron</button>
+</form>
+*/
+
+/* 3
+<form action="/checkout" method="post">
+  <input hidden name="city" value="423" />
+  <input hidden name="street" value="Universitetskaya" />
+  <input hidden name="building" value="1" />
+  <input hidden name="appartment" value="1" />
+  <button type="submit" class="btn btn-link">http://aliexpress.com/best/cheap/iron</button>
+</form>
+*/
+
+/* 2
+<form action="/checkout" method="post">
+  <input name="city" value="423" />
+  <input hidden name="street" value="Universitetskaya" />
+  <input hidden name="building" value="1" />
+  <input hidden name="appartment" value="1" />
+  <button type="submit">http://aliexpress.com/best/cheap/iron</button>
+</form>
+*/
+
+/* 1
+<form action="/checkout" method="post">
+  <input name="city" value="423" />
+  <input name="street" value="Universitetskaya" />
+  <input name="building" value="1" />
+  <input name="appartment" value="1" />
+  <button type="submit">http://aliexpress.com/best/cheap/iron</button>
 </form>
 */
